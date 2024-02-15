d5c7591a7424ab783f74c7b50c0220a49d05ad92
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-02-15 00:07:48 -0500

    create 16x16 grid

---
+++ main.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("container");
+
+function grid(rows, cols){
+    for(let i = 0; i < rows; i++){
+        let cell = document.createElement("div");
+        container.appendChild(cell);
+        cell.style.border = "none";
+        cell.style.padding = "0px";
+        for(let x = 0; x < cols; x++){
+            let cell1 = document.createElement("div");
+            cell.appendChild(cell1);
+        };
+    };
+};
+
+grid(16, 16);
