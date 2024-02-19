892f5f9b47432a7df0f48e9b2af9db4d5f6ee4f2
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-02-19 18:22:34 -0500

    allow grid resizing

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+let cellWidthPercentage = 0;
+let cellHeightPercentage = 0;
 
 function grid(rows, cols){
     for(let i = 0; i < rows; i++){
@@ -6,6 +8,13 @@ function grid(rows, cols){
         container.appendChild(cell);
         cell.style.border = "none";
         cell.style.padding = "0px";
+
+        //calculate the percentage width for each cell
+        let cellWidthPercentage = 100 / cols;
+        let cellHeightPercentage = 100 / rows;
+        cell.style.width = `${cellWidthPercentage}%`;
+        cell.style.height = `${cellHeightPercentage}%`;
+
         for(let x = 0; x < cols; x++){
             let cell1 = document.createElement("div");
             cell.appendChild(cell1);
@@ -19,3 +28,18 @@ function grid(rows, cols){
 };
 
 grid(16, 16);
+
+let gridSize = 0;
+
+let resize = document.getElementById("resize");
+resize.addEventListener("click", function (){
+    container.replaceChildren();
+    gridSize=prompt("Enter the grid size you would like to use max 100.");
+    grid.clear;
+
+    cellWidthPercentage = 100 / gridSize;
+    cellHeightPercentage = 100 / gridSize;
+    grid(gridSize, gridSize);
+});
+
+
